833d8315462af912f067092c957bd8fe1204ab4a
Douglas A.C <douglas.affc@gmail.com>
2025-07-15 15:02:32 +0000

    Adding git inialization to the development container.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/bash
 
+# RESTORING PROJECT DEPENDENCIES
 dotnet restore --disable-parallel && dotnet workload restore && dotnet tool restore;
+
+# CONFIURING AND MAKING AVAILABLE GIT SERVICES
 apt-get update && apt-get install -y openssh-client;
+git config --global --add safe.directory /workspace;
+git config --global --add safe.directory /workspace/.git;
