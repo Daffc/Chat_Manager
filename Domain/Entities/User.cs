ec91cc40851b8e539f88454948357c99cbd76bfb
Douglas A.C <douglas.affc@gmail.com>
2025-07-15 17:38:10 +0000

    Redefining User model, correcting Non-nullable properties.

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -3,13 +3,22 @@ using System.ComponentModel.DataAnnotations;
 
 public class User
 {
+    public User(string nickName, string firstName, string lastName, string email, string password)
+    {
+        NickName = nickName;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+    }
+
     [Key]
-    public int Id { get; set; }
-    public string NickName { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
+    public int Id { get; private set; }
+    public string NickName { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
     public DateTime? DeletedAt { get; set; }
